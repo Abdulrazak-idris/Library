e54caa6610236d4350308296c861b5cbd27dcd5e
Abdulrazak Idris <Abdulrazakidrisyahaya@gmail.com>
2024-09-08 22:24:18 +0100

    Added read status buttom

--- script.js
+++ script.js
@@ -24,14 +24,23 @@ function showBooks(library) {
     for (const book of library) {
         const cardContainer = document.createElement("div");
         const newDiv = document.createElement("div");
+        const readOrDelete = document.createElement("div");
         const btn = document.createElement("button");
+        const readBtn = document.createElement("button");
         cardContainer.classList.add('card-container');
         newDiv.classList.add('card');
+        readOrDelete.classList.add('readOrDelete-container');
         btn.classList.add('btn', 'btn-delete');
+        readBtn.classList.add('btn', 'btn-read');
         newDiv.textContent = book;
         btn.textContent = "Delete";
-        cardContainer.appendChild(newDiv);
-        cardContainer.appendChild(btn);
+        readBtn.textContent = "Read Status";
+        readOrDelete.appendChild(readBtn);
+        readOrDelete.appendChild(btn);
+        const childElements = [newDiv, readOrDelete];
+        for (let i = 0; i < childElements.length; i++) {
+            cardContainer.appendChild(childElements[i]);
+        }
         page.appendChild(cardContainer);
     }
 }
@@ -41,7 +50,7 @@ showBooks(myLibrary)
 
 //Input form
 const btn = document.querySelector(".btn-addNewBook");
-btn.addEventListener("click", function (event) {
+btn.addEventListener("click", (event) => {
     alert("wow")
     event.preventDefault();
 })
@@ -61,10 +70,14 @@ newBook.addEventListener("click", function () {
 })
 
 const deleteBtns = document.querySelectorAll(".btn-delete");
+const readBtns = document.querySelectorAll(".btn-read");
 const deleteBtn = Array.from(deleteBtns);
+const readBtn = Array.from(readBtns);
 for (const btn of deleteBtn) {
     btn.addEventListener("click", (event) => {
         const parent = event.target.parentNode;
-        parent.remove();
+        const grandParent = parent.parentNode;
+        grandParent.remove();
     })
 }
+
