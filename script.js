574d97fe54839393b3114df5c375ae8ac258cc52
Abdulrazak Idris <Abdulrazakidrisyahaya@gmail.com>
2024-09-10 18:30:19 +0100

    Create a save book functionality

--- script.js
+++ script.js
@@ -4,60 +4,35 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     this.info = function () {
-        return `${this.title} by ${this.author}, ${this.pages} ${this.read}`
+        return `${this.title} by ${this.author}, ${this.pages} pages ${this.read}`
     }
 }
 
 
 //My array of books.
-const myLibrary = ['Abdulrazak Idris', "Am trainig", 283, "pages read", 'Abdulrazak Idris', "Am trainig", 283, "pages read"];
+const myLibrary = [];
 
-// Get all Books.
-function addBookToLibrary(book) {
-    myLybrary.push(book)
-}
-
-
-//Display each book.
-function showBooks(library) {
-    const page = document.querySelector(".page");
-    for (const book of library) {
-        const cardContainer = document.createElement("div");
-        const newDiv = document.createElement("div");
-        const readOrDelete = document.createElement("div");
-        const btn = document.createElement("button");
-        const readBtn = document.createElement("button");
-        cardContainer.classList.add('card-container');
-        newDiv.classList.add('card');
-        readOrDelete.classList.add('readOrDelete-container');
-        btn.classList.add('btn', 'btn-delete');
-        readBtn.classList.add('btn', 'btn-read');
-        newDiv.textContent = book;
-        btn.textContent = "Delete";
-        readBtn.textContent = "Read Status";
-        readOrDelete.appendChild(readBtn);
-        readOrDelete.appendChild(btn);
-        const childElements = [newDiv, readOrDelete];
-        for (let i = 0; i < childElements.length; i++) {
-            cardContainer.appendChild(childElements[i]);
-        }
-        page.appendChild(cardContainer);
-    }
-}
-
-// addBookToLibrary(mySampleBook);
-showBooks(myLibrary)
 
 //Input form
 const btn = document.querySelector(".btn-addNewBook");
 btn.addEventListener("click", (event) => {
     const inputs = document.querySelectorAll("input");
     const arrayOfInputs = Array.from(inputs);
+    const inputValues = [];
     for (const input of arrayOfInputs) {
-        alert(input.previousElementSibling.innerHTML)
+        if (input.value) {
+            inputValues.push(input.value)
+        } else {
+            alert(`Nothing Entered in ${input.previousElementSibling.innerHTML}`)
+        }
     }
 
-    // const currentBook = new Book();
+    const currentBook = new Book(...inputValues);
+    addBookToLibrary(currentBook)
+    
+    // addBookToLibrary(mySampleBook);
+    showBooks(myLibrary)
+
     event.preventDefault();
 })
 
@@ -87,8 +62,33 @@ for (const btn of deleteBtn) {
     })
 }
 
-for (const item of readBtn) {
-    item.addEventListener("click", (event) => {
 
-    })
+//Display each book.
+function showBooks(library) {
+    const page = document.querySelector(".page");
+        const cardContainer = document.createElement("div");
+        const newDiv = document.createElement("div");
+        const readOrDelete = document.createElement("div");
+        const btn = document.createElement("button");
+        const readBtn = document.createElement("button");
+        cardContainer.classList.add('card-container');
+        newDiv.classList.add('card');
+        readOrDelete.classList.add('readOrDelete-container');
+        btn.classList.add('btn', 'btn-delete');
+        readBtn.classList.add('btn', 'btn-read');
+        newDiv.textContent = library[library.length - 1].info();
+        btn.textContent = "Delete";
+        readBtn.textContent = "Read Status";
+        readOrDelete.appendChild(readBtn);
+        readOrDelete.appendChild(btn);
+        const childElements = [newDiv, readOrDelete];
+        for (let i = 0; i < childElements.length; i++) {
+            cardContainer.appendChild(childElements[i]);
+        }
+        page.appendChild(cardContainer);
+}
+
+// Get all Books.
+function addBookToLibrary(book) {
+    myLibrary.push(book)
 }
